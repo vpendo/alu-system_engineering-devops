6aea77c3069e92b9d30d1ea22d78ab02128c23de
Pendo Vestine <144009226+vpendo@users.noreply.github.com>
2024-03-26 11:39:02 +0200

    done

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,8 @@
-S so as to enable the user 'holberton' to login & open a file without error messages
+
+# puppet code to configure OS so as to enable the user 'holberton' to login & open a file without error messages
 
 exec { 'replace_hard_soft_limit_for_holberton_user':
   path    => '/usr/bin:/usr/sbin:/bin',
   command => 'sed -i "/holberton hard nofile/c\holberton hard nofile 4096" /etc/security/limits.conf;\
 sed -i "/holberton soft nofile/c\holberton soft nofile 1024" /etc/security/limits.conf',
 }
-
